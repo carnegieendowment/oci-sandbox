669e03c538e36c69bedddd3b5fea7afeea811f6b
sawojcicki <sawojcicki@gmail.com>
2017-07-28 18:50:19 -0700

    Update precalc.js

--- .build_scripts/precalc.js
+++ .build_scripts/precalc.js
@@ -13,25 +13,46 @@ Oci.data.globalExtents = {}
 
 // Load all data based on metadata
 var metadata = Oci.data.metadata
+var vi = utils.trimMetadataArray(metadata.venting.split(','));
 var si = utils.trimMetadataArray(metadata.solarSteam.split(','));
 var wi = utils.trimMetadataArray(metadata.water.split(','));
 var fi = utils.trimMetadataArray(metadata.flare.split(','));
 var ri = utils.trimMetadataArray(metadata.refinery.split(','));
-var li = [1, 0];
+var pi = utils.trimMetadataArray(metadata.fugitives.split(','));
+var gi = [1,0];
+var zi = [1,0];
+var ai = [1,0];
+var ei = [1,0];
+var hi = [1,0];
+var li = [1,0];
 
-si.forEach(function (_, s) {
-  wi.forEach(function (_, w) {
-    fi.forEach(function (_, f) {
-      var temp = JSON.parse(fs.readFileSync('app/assets/data/opgee/opgee_run' + s + w + f + '.json'));
-      Oci.data.opgee['run' + s + w + f] = temp;
+gi.forEach(function (_, g) {
+  pi.forEach(function (_, p) {
+    vi.forEach(function (_, v) {
+      si.forEach(function (_, s) {
+        wi.forEach(function (_, w) {
+         fi.forEach(function (_, f) {
+          var temp = JSON.parse(fs.readFileSync('app/assets/data/opgee/opgee_run' + s + w + f + '.json'));
+          Oci.data.opgee['run' + s + w + f] = temp;
+         });
+        });
+      });
     });
   });
 });
-
-ri.forEach(function (_, r) {
-  li.forEach(function (_, l) {
-    var temp = JSON.parse(fs.readFileSync('app/assets/data/prelim/prelim_run' + r + l + '.json'));
-    Oci.data.prelim['run' + r + l] = temp;
+      
+hi.forEach(function (_, h) {
+  ei.forEach (function (_, e)}
+    zi.forEach(function (_, z) {
+      ai.forEach(function (_, a) {
+        ri.forEach(function (_, r) {
+          li.forEach(function (_, l) {
+            var temp = JSON.parse(fs.readFileSync('app/assets/data/prelim/prelim_run' + z + a + r + l + '.json'));
+            Oci.data.prelim['run' + z + a + r + l] = temp;
+          });
+        });
+      });
+    });
   });
 });
 
